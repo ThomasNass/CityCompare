8fc59739e224f67156bf5a1eff2a030958a65351
Thomas <tn900323@gmail.com>
2022-06-15 08:43:43 +0200

    trying with res.render

--- server/index.mjs
+++ server/index.mjs
@@ -17,7 +17,7 @@ const route = express.static("./dist")
 
 
 app.get("/", (req, res) => {
-    res.send("/app.html")
+    res.render("/app.html")
 })
 
 app.get("/api/hitta/:company/:municipality", async (req, res) => {
